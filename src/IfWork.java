36fdf660a9333fa82efe3cb7eae62f396ff895ed
Bishworaj Poudel <iambrp.tech@gmail.com>
2021-12-10 20:50:22 +0545

    IfWork

---
+++ src/IfWork.java
@@ -0,0 +1,10 @@
+public class IfWork {
+    public static void main(String[] args) {
+        double price = 500;
+        if(price <=500){
+            System.out.println("Price is less than or equal to 500");
+        }else{
+            System.out.println("Price is more than 500");
+        }
+    }
+}

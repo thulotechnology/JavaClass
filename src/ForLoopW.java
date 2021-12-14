e74cabbc566045d26ef95e5507d5cc34836bc3dc
Bishworaj Poudel <iambrp.tech@gmail.com>
2021-12-14 20:54:03 +0545

    Okay

---
+++ src/ForLoopW.java
@@ -0,0 +1,19 @@
+public class ForLoopW {
+    public static void main(String[] args) {
+        // Working on for loop
+//       for(int i=1; i<=50; i++){
+//           System.out.println("Number is "+i + ".");
+//       }
+
+        // 100 - 1
+//        for(int i=100; i>=1; i--){
+//            System.out.println(i);
+//        }
+
+      // Print your name 1000 Times
+      for(int i=1; i<1001; i++){
+          System.out.println(i+". Shiva Adhikari");
+      }
+
+    }
+}
---
+++ src/MultiplyWork.java
@@ -0,0 +1,10 @@
+public class MultiplyWork {
+    public static void main(String[] args) {
+        int multi = 80;
+
+        for(int i =1; i<=10; i++){
+            System.out.println(multi+" * "+i+" :"+(multi*i));
+        }
+
+    }
+}
